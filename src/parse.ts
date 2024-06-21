5a4573b48759166be01e702b4ff204ccd017b6b6
Nicolai Schmid <nicolai@schmid.uno>
2024-06-21 18:11:24 +0200

    docs: jsdoc

--- src/parse.ts
+++ src/parse.ts
@@ -2,6 +2,19 @@ import { ZodSchema, z } from "zod";
 import axios from "axios";
 import zodToJsonSchema from "zod-to-json-schema";
 
+/**
+ * Parses the provided payload according to the specified Zod schema.
+ *
+ * @template T - The Zod schema type.
+ * @param {T} zod - The Zod schema to validate against.
+ * @param {object} example - An example object to validate the schema.
+ * @param {string} payload - The payload to be parsed.
+ * @param {string} instruction - Additional instructions for the parsing process.
+ * @param {object} options - Options object.
+ * @param {string} [options.apiKey] - Optional API key for authentication, otherwise COSMO_AI_KEY env var is used
+ * @returns {Promise<z.infer<T>>} - The parsed payload as inferred by the Zod schema.
+ * @throws {Error} - Throws an error if API key is missing or if the example is invalid.
+ */
 export default async function parse<T extends ZodSchema>(
   zod: T,
   example: object,
